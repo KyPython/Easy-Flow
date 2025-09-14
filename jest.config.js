67f772c889e547971a170a5cfbba5e6cd3a9491b
KyPython <kyjahntsmith@gmail.com>
2025-09-14 00:32:23 -0400

    Test

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {
-        '^.+\\.jsx?$': ['babel-jest', { 
-            configFile: './babel.config.js' // explicitly specify Babel config file
-        }]
-    },
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-};
