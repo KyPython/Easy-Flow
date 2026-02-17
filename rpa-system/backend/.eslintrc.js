beccef519442a55e200e174d932baa0630b02bfa
KyPython <kyjahntsmith@gmail.com>
2026-02-16 23:07:07 -0500

    chore: relax ESLint for backend scripts/tests (reduce no-console/no-unused-vars noise)

--- rpa-system/backend/.eslintrc.js
+++ rpa-system/backend/.eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
+  root: true,
+  overrides: [
+    {
+      files: [
+        'scripts/**',
+        'scripts/*.js',
+        'seed-*.js',
+        'run-*.js',
+        'tests/**',
+        'tests/*.js',
+        'migrations/**',
+        'bin/**'
+      ],
+      rules: {
+        'no-console': 'off',
+        'no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^(getLogger|getSupabase|getSupabaseOrThrow|createClient|createContextLogger|getFrontendConfigInfo|createLogger|_?logger|_?getLogger)$'
+          }
+        ]
+      }
+    },
+    {
+      files: ['**/*.test.js', '**/*.e2e.test.js'],
+      rules: {
+        'no-console': 'off'
+      }
+    }
+  ]
+};
+module.exports = {
  env: {
  node: true,
  es2021: true,
